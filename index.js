60782a228c298a722fa5808ca8fdadc0568a25b8
Viktoriia <freshitpte@gmail.com>
2024-09-29 17:24:34 +0300

    Add  then, cb, async

--- index.js
+++ index.js
@@ -13,3 +13,28 @@ const pool = new Pool(connectionOptions);
 
 // Завершити з'єднання з БД при завершенні роботи застосунку
 process.on('beforeExit', () => pool.end());
+
+// ---------------------------------------
+// promises then/catch
+pool
+  .query('SELECT CURRENT_DATE')
+  .then(result => console.log('then:', result.rows[0]))
+  .catch(err => console.log(err));
+
+// cb
+pool.query('SELECT CURRENT_DATE', (err, result) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('cb:', result.rows[0]);
+});
+
+// promises async/await
+(async function () {
+  try {
+    const result = await pool.query('SELECT CURRENT_DATE');
+    console.log('await:', result.rows[0]);
+  } catch (err) {
+    console.log(err);
+  }
+})();
