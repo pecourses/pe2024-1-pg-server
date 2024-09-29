1802be39dba7d02e6203e3825ca9589aa1a79db6
Viktoriia <freshitpte@gmail.com>
2024-09-29 17:23:36 +0300

    Add pg connection

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+// TODO: move to configs.js / process.evv
+const connectionOptions = {
+  user: 'postgres',
+  password: 'admin',
+  host: 'localhost',
+  port: 5432,
+  database: 'phones_sales',
+};
+
+const pool = new Pool(connectionOptions);
+
+// Завершити з'єднання з БД при завершенні роботи застосунку
+process.on('beforeExit', () => pool.end());
